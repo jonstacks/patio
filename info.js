50ba7e9186ce9273844363103b82c85e61241454
Jonathan Stacks <jonstacks13@gmail.com>
2019-02-23 13:16:11 -0600

    WIP

--- info.js
+++ info.js
@@ -1,31 +1,35 @@
 
 var assert = require('assert'),
-helper = require("./test/helpers/helper"),
-MockDatabase = helper.MockDatabase,
-MockDataset = helper.MockDataset,
-patio = require("./lib"),
-Dataset = patio.Dataset,
-sql = patio.SQL;
+    helper = require("./test/helpers/helper"),
+    MockDatabase = helper.MockDatabase,
+    MockDataset = helper.MockDataset,
+    patio = require("./lib"),
+    Dataset = patio.Dataset,
+    sql = patio.SQL,
+    Identifier = sql.Identifier;
 
-var ds = new MockDatabase().from("t");
 
-var dataset = new Dataset().from("test");
+var i = sql.Identifier("dada ASC");
 
-console.log(dataset.order(sql['dada ASC']));
+// var ds = new MockDatabase().from("t");
 
-console.log("TEST 1 ----------------");
-assert.equal(dataset.order(sql['dada ASC']).sql, 'SELECT * FROM test ORDER BY dada ASC');
+// var dataset = new Dataset().from("test");
 
-console.log("TEST 2 ----------------");
-assert.equal(ds.filter(sql.a.sqlSubscript(sql.b, 3)).qualifyToFirstSource().sql, 'SELECT t.* FROM t WHERE t.a[t.b, 3]');
+// console.log(dataset.order(sql['dada ASC']));
 
-var d = new MockDataset().from("items");
-d.quoteIdentifiers = true;
+// console.log("TEST 1 ----------------");
+// assert.equal(dataset.order(sql['dada ASC']).sql, 'SELECT * FROM test ORDER BY dada ASC');
 
-console.log("TEST 3 ------------------");
-assert.equal(d.join("categories", sql.active).sql, 'SELECT * FROM "items" INNER JOIN "categories" ON "active"');
+// console.log("TEST 2 ----------------");
+// assert.equal(ds.filter(sql.a.sqlSubscript(sql.b, 3)).qualifyToFirstSource().sql, 'SELECT t.* FROM t WHERE t.a[t.b, 3]');
 
-console.log("Done!");
+// var d = new MockDataset().from("items");
+// d.quoteIdentifiers = true;
+
+// console.log("TEST 3 ------------------");
+// assert.equal(d.join("categories", sql.active).sql, 'SELECT * FROM "items" INNER JOIN "categories" ON "active"');
+
+// console.log("Done!");
 // let f = function(name) {
 //     return "hello" +  name;
 // }
